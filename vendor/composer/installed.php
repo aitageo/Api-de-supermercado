fb247fd689949cc1ea0bf4d29700d6d5fe31b5ef
aitageo <aitageo1980@hotmail.com>
2024-11-28 19:20:23 -0500

    configurando para render

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '56486583939536b98e4188db7fb89678d4d7b985',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '56486583939536b98e4188db7fb89678d4d7b985',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
